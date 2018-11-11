fd00549e96cb738d7ae3653e29a3b6e12d83c73c
Martin <martin.liriano@gmail.com>
2018-11-11 15:50:16 -0500

    added login button to login screen

--- components/login.js
+++ components/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, StyleSheet } from 'react-native';
+import { Text, View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default class login extends Component {
   constructor(props) {
@@ -7,16 +7,42 @@ export default class login extends Component {
     this.state = {username : '', password : ''}
   }
 
+  login() {
+
+  }
+
   render() {
     return (
       <View style = {{padding : 10}}>
-        <TextInput style = {{height : 40, borderWidth : 1, padding : 10}}
+        <TextInput style = {styles.inputContainer}
                     placeholder = "Username"
                     onChangeText = {(username) => this.setState({username})}/>
-        <TextInput style = {{height : 40, borderWidth : 1, padding : 10}}
+        <TextInput style = {styles.inputContainer}
                     placeholder = "Password"
                     onChangeText = {(password) => this.setState({password})}/>
+        <TouchableOpacity onPress = {login}
+                style = {styles.buttonContainer}>
+            <Text>Login</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    height : 40,
+    borderWidth : 0.25,
+    padding : 10,
+    borderRadius : 25,
+    width : 300,
+    margin : 5
+  },
+  buttonContainer: {
+    alignItems : 'center',
+    alignSelf : 'center',
+    backgroundColor : '#DDDDDD',
+    padding : 10,
+    width : 100
+  },
+});
