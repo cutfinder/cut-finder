cf955ed62e10592e1bfc02a9ea1b71c47af25212
Anirudh Pillutla <a.v.pillutla@gmail.com>
2019-07-30 21:03:25 -0400

    making api call and rendering data

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+
+var testString = "Hello, World!";
 
 export default class App extends React.Component {
+  state = {
+    barber : ""
+  }
+  componentDidMount(){
+    axios.get('https://kysolqk4pc.execute-api.us-east-1.amazonaws.com/beta/findBarber/')
+      .then(response => {
+        const barber = response.data;
+        this.setState({ barber });
+      })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{this.state.barber.Name}</Text>
+        <Text>{this.state.barber.Location}</Text>
+        <Text>{this.state.barber.Salon}</Text>
+        <Text>{this.state.barber.Portfolio}</Text>
       </View>
     );
   }
